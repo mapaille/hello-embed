4781fd840de98b9b650cc169d67ee817a7b39679
mapaille <mpaille@live.ca>
2026-02-24 21:27:52 -0500

    Big cleanup.

--- src/system.rs
+++ src/system.rs
@@ -1,11 +1,8 @@
-use crate::app::hardware::Hardware;
-use crate::app::state;
+use crate::app::RtcHandler;
 use crate::peripherals::{gpio, rtc};
-use crate::programs::ProgramId;
-use crate::traits::{Cancellable, Pressable};
 use crate::{clock, interrupt, power};
 
-const HARDWARE: Hardware = Hardware::new();
+static RTC_HANDLER: RtcHandler = RtcHandler::new();
 
 pub fn init() {
     clock::use_high_frequency_clock();
@@ -18,26 +15,5 @@ pub fn init() {
 
 #[inline]
 fn rtc_callback() {
-    let program_id = state::get_program_id();
-    let new_program_id = determine_program_id_from_buttons();
-
-    if program_id != new_program_id {
-        state::set_program_id(new_program_id);
-        state::get_cancellation_token().cancel();
-    }
-}
-
-#[inline]
-fn determine_program_id_from_buttons() -> ProgramId {
-    if HARDWARE.left_button.is_pressed() && HARDWARE.right_button.is_pressed() {
-        ProgramId::Startup
-    } else if HARDWARE.left_button.is_pressed() {
-        ProgramId::Love
-    } else if HARDWARE.right_button.is_pressed() {
-        ProgramId::Temperature
-    } else if HARDWARE.touch_button.is_pressed() {
-        ProgramId::Startup
-    } else {
-        state::get_program_id()
-    }
+    RTC_HANDLER.on_rtc();
 }
